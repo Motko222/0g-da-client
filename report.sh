6d764d65ab41f10c234c1a65a4ed4a5603dffd02
Motko <77466238+Motko222@users.noreply.github.com>
2025-07-21 22:01:03 +0200

    Update report.sh

--- report.sh
+++ report.sh
@@ -6,16 +6,6 @@ json=~/logs/report-$folder
 source ~/.bash_profile
 source $path/env
 
-
-#get RPC addresses
-node_rpc=$(cat ~/0g-da-node/config.toml | grep '^grpc_listen_address =' | tail -1 | awk '{print $3}' | sed 's/"//g')
-chain_rpc=$(cat ~/0g-da-node/config.toml | grep '^eth_rpc_endpoint =' | tail -1 | awk '{print $3}' | sed 's/"//g')
-disperser_rpc=localhost:51001
-
-#get da node info
-cd ~/0g-da-node/target/release
-node_version=$(cat /root/logs/da-node-version )
-cd ~
 node_status=$(./grpcurl --plaintext localhost:51001 grpc.health.v1.Health/Check | jq -r .status )
 docker_status=$(docker inspect 0g-da-client | jq -r .[].State.Status)
 
