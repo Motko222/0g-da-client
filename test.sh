296152412339334a71a426982f2b42e7553d6be1
Motko <77466238+Motko222@users.noreply.github.com>
2025-07-21 21:35:23 +0200

    Update test.sh

--- test.sh
+++ test.sh
@@ -4,12 +4,12 @@ path=$(cd -- $(dirname -- "${BASH_SOURCE[0]}") && pwd)
 folder=$(echo $path | awk -F/ '{print $NF}')
 cd $path
 source $path/env
-c=$(cat rpc | wc -l)
+c=$(cat rpc-test | wc -l)
 
 echo "------------------------"
 for (( i=1;i<=$c;i++ ))
 do
-   rpc=$(cat rpc | head -$i | tail -1)
+   rpc=$(cat rpc-test | head -$i | tail -1)
    height=$(curl -sX POST $rpc -H "Content-Type: application/json" -d '{"jsonrpc":"2.0","method":"eth_blockNumber","params":[],"id":1}' | jq -r .result)
    printf "%s %-60s %s \n" $i $rpc $height
 done
