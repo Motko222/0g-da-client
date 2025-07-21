8aaa48cacb120ecc2ecf6e360de02f01a37cb34e
Motko <77466238+Motko222@users.noreply.github.com>
2025-07-21 21:28:54 +0200

    Update config.sh

--- config.sh
+++ config.sh
@@ -2,6 +2,8 @@
 
 path=$(cd -- $(dirname -- "${BASH_SOURCE[0]}") && pwd) 
 folder=$(echo $path | awk -F/ '{print $NF}')
+cd $path
+[ -f envfile.env ] || cp envfile.sample envfile.env
 
 cd $path
 nano envfile.env
