85d1568d10d7fa13bd85b451714863835413377c
Motko <77466238+Motko222@users.noreply.github.com>
2025-07-21 21:42:47 +0200

    Update test.sh

--- test.sh
+++ test.sh
@@ -11,7 +11,7 @@ for (( i=1;i<=$c;i++ ))
 do
    rpc=$(cat test-rpc | head -$i | tail -1)
    status=$(/root/grpcurl --plaintext $rpc grpc.health.v1.Health/Check | jq -r .status)
-   printf "%s %-60s %s \n" $i $rpc $height
+   printf "%s %-60s %s \n" $i $rpc $status
 done
 
 echo "------------------------"
