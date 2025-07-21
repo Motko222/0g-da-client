ebfde161c33d7d7cda0cf254bc2b03e21bd818c7
Motko <77466238+Motko222@users.noreply.github.com>
2025-07-21 21:40:39 +0200

    Update test.sh

--- test.sh
+++ test.sh
@@ -4,13 +4,13 @@ path=$(cd -- $(dirname -- "${BASH_SOURCE[0]}") && pwd)
 folder=$(echo $path | awk -F/ '{print $NF}')
 cd $path
 source $path/env
-c=$(cat rpc-test | wc -l)
+c=$(cat test-rpc | wc -l)
 
 echo "------------------------"
 for (( i=1;i<=$c;i++ ))
 do
-   rpc=$(cat rpc-test | head -$i | tail -1)
-   height=$(curl -sX POST $rpc -H "Content-Type: application/json" -d '{"jsonrpc":"2.0","method":"eth_blockNumber","params":[],"id":1}' | jq -r .result)
+   rpc=$(cat test-rpc | head -$i | tail -1)
+   status=$(/root/grpcurl --plaintext $rpc grpc.health.v1.Health/Check | jq -r .status)
    printf "%s %-60s %s \n" $i $rpc $height
 done
 
